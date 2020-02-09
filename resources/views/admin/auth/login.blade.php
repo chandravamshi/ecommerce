1add41f014cdfa2b2f20809d92bf06bded0069b2
chandravamshi <dasyamchandu@gmail.com>
2020-02-09 18:16:14 +0100

    dufi

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -38,7 +38,7 @@
                 </div>
             </div>
             <div class="form-group btn-container">
-                <button class="btn btn-primary btn-block" type="submit"><i class="fa fa-sign-in fa-lg fa-fw"></i>SIGN IN</button>
+                <button class="btn btn-primary btn-block" id="login-button" type="submit"><i class="fa fa-sign-in fa-lg fa-fw"></i>SIGN IN</button>
             </div>
         </form>
     </div>
--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -3,13 +3,14 @@
 namespace Tests;
 
 use Facebook\WebDriver\Chrome\ChromeOptions;
-use Laravel\Dusk\TestCase as BaseTestCase;
-use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\TestCase as BaseTestCase;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
     /**
      * Prepare for Dusk test execution.
      *
@@ -20,6 +21,7 @@ abstract class DuskTestCase extends BaseTestCase
     {
         static::startChromeDriver();
     }
+
     /**
      * Create the RemoteWebDriver instance.
      *
@@ -28,14 +30,16 @@ abstract class DuskTestCase extends BaseTestCase
     protected function driver()
     {
         $options = (new ChromeOptions)->addArguments([
-            '--disable-gpu',
-            '--headless',
-            '--no-sandbox'
+            //'--disable-gpu',
+            //'--headless',
+            '--window-size=1920,1080',
+            'detach=true',
         ]);
+    
         return RemoteWebDriver::create(
             'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
-            ChromeOptions::CAPABILITY, $options
-        )
+                ChromeOptions::CAPABILITY, $options
+            )
         );
     }
 }
